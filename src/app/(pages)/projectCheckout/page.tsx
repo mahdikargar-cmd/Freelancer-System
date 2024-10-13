867a168dc9cf03db5e08a3f391e603b393d7a506
mahdi <www.mahdikargar@gmail.com>
2024-10-13 14:14:51 +0330

    some bug fixed in ProjectCheckoutPage

--- src/app/(pages)/projectCheckout/page.tsx
+++ src/app/(pages)/projectCheckout/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 function ProjectCheckout() {
     const [modal, setModal] = useState(false);
@@ -9,7 +9,6 @@ function ProjectCheckout() {
     }
 
     const closeModal = (e) => {
-        // بستن مدال با کلیک بیرون از آن
         if (e.target.id === 'modalBackground') {
             setModal(false);
         }
@@ -22,14 +21,18 @@ function ProjectCheckout() {
                     <div className={'grid grid-cols-12'}>
                         <div className={'col-span-12 flex justify-between items-center'}>
                             <p className={'font-bold text-[20px]'}>ربات رزرو بلیط قطار با کیفیت و سرعت</p>
-                            <button onClick={changeModal} className={'bg-blue-500 text-white p-2 rounded-md'}>پیشنهاد روی پروژه</button>
+                            <button onClick={changeModal} className={'bg-blue-500 text-white p-2 rounded-md'}>پیشنهاد
+                                روی پروژه
+                            </button>
                         </div>
                     </div>
                     <div className={'grid grid-cols-12 mt-8'}>
                         <div className={'col-span-12 flex justify-start leading-8 text-[18px]'}>
                             <p>
-                                سلام و وقت بخیر، به یک فرد خلاق و وقت‌شناس برای طراحی سایت نیاز داریم که بتواند یک پلتفرم
-                                ساخت منوی کافه و رستوران با قابلیت اسکن QR کد ایجاد کند. سایت مورد نظر باید تمامی امکانات
+                                سلام و وقت بخیر، به یک فرد خلاق و وقت‌شناس برای طراحی سایت نیاز داریم که بتواند یک
+                                پلتفرم
+                                ساخت منوی کافه و رستوران با قابلیت اسکن QR کد ایجاد کند. سایت مورد نظر باید تمامی
+                                امکانات
                                 سایت‌های زیر را داشته باشد (و حتی بیشتر):
 
                                 - https://menew.pw/
@@ -37,9 +40,12 @@ function ProjectCheckout() {
                                 - https://qrbama.com/
                                 - https://menusaz.com/
 
-                                طراحی باید کاملاً کاربرپسند، خلاقانه و به‌روز باشد. پس از انتخاب، توضیحات دقیق‌تری درباره
-                                ویژگی‌ها و انتظارات ارائه خواهد شد. اولویت ما در این پروژه، داشتن خلاقیت بالا و تحویل به موقع است.
-                                لطفاً اگر توانایی‌های لازم را دارید و می‌توانید کیفیت مورد نظر را ارائه دهید، به ما پیام دهید.
+                                طراحی باید کاملاً کاربرپسند، خلاقانه و به‌روز باشد. پس از انتخاب، توضیحات دقیق‌تری
+                                درباره
+                                ویژگی‌ها و انتظارات ارائه خواهد شد. اولویت ما در این پروژه، داشتن خلاقیت بالا و تحویل به
+                                موقع است.
+                                لطفاً اگر توانایی‌های لازم را دارید و می‌توانید کیفیت مورد نظر را ارائه دهید، به ما پیام
+                                دهید.
 
                                 با تشکر از شما.
                             </p>
@@ -49,9 +55,11 @@ function ProjectCheckout() {
                         <div className={'col-span-12 flex justify-between'}>
                             <p className={'font-bold'}>مهارت های مورد نیاز :</p>
                             <p className={'border-b-blue-500 border-2 pl-2 ps-2 rounded-full'}>الگوریتم</p>
-                            <p className={'border-b-blue-500 border-2 pl-2 ps-2 rounded-full'}>برنامه نویسی سی پلاس پلاس (C++)</p>
+                            <p className={'border-b-blue-500 border-2 pl-2 ps-2 rounded-full'}>برنامه نویسی سی پلاس پلاس
+                                (C++)</p>
                             <p className={'border-b-blue-500 border-2 pl-2 ps-2 rounded-full'}>جاوا (Java)</p>
-                            <p className={'border-b-blue-500 border-2 pl-2 ps-2 rounded-full'}>جاوا اسکریپت (JavaScript)</p>
+                            <p className={'border-b-blue-500 border-2 pl-2 ps-2 rounded-full'}>جاوا اسکریپت
+                                (JavaScript)</p>
                         </div>
                     </div>
                 </div>
@@ -63,16 +71,40 @@ function ProjectCheckout() {
                     onClick={closeModal}
                     className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
                 >
-                    <div className="bg-amber-50 w-[600px] p-6 rounded-lg text-center relative">
-                        <button
-                            onClick={changeModal}
-                            className="absolute top-2 right-2 bg-red-500 text-white p-2 rounded-full"
-                        >
-                            ×
-                        </button>
-                        <h2 className="text-xl font-bold mb-4">فرم پیشنهاد</h2>
-                        <p>متن خود را وارد کنید:</p>
-                        <input type="text" className="w-full p-2 mt-2 border rounded" />
+                    <div className="bg-amber-50 w-[800px] h-[700px] p-6 rounded-lg text-center  ">
+
+                        <div className="grid grid-cols-12 m-2">
+
+                            <div className={'col-span-6 p-2 flex justify-between'}>
+                                <p>
+                                    موضوع
+                                </p>
+                                <input type="text" className={'bg-gray-400 rounded'}/>
+                            </div>
+                            <div className={'col-span-6 p-2 flex justify-between'}>
+                                <p> مدت زمان انجام پروژه</p>
+                                <input type="number" className={'bg-gray-400 rounded'}/>
+                            </div>
+                        </div>
+                        <div className={'grid grid-cols-12'}>
+                            <div className={'col-span-12 mt-8 flex justify-evenly items-center'}>
+                                <p>چرا کارفرما باید به شما اعتماد کند؟</p>
+                                <textarea className={'bg-gray-400  w-[500px] h-[300px] rounded'}/>
+
+                            </div>
+                        </div>
+                        <div className={'grid grid-cols-12'}>
+                            <div className={'col-span-12 mt-8 flex justify-evenly items-center '}>
+                                <p>قیمت پیشنهادی تون روی پروژه</p>
+                                <input type="number" className={'bg-gray-400 rounded'}/>
+                            </div>
+                        </div>
+                        <div className={'grid grid-cols-12 '}>
+                            <div className={'col-span-12 mt-32 flex justify-evenly'}>
+                                <button onClick={closeModal} className={'bg-red-500 text-white p-1 ps-3 pl-3 rounded'}>لغو</button>
+                                <button className={'bg-green-500 text-white p-1 ps-3 pl-3 rounded'}>ثبت برروی پروژه</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
